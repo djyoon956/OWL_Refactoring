733614cca0499f8c21195a613d4910ac6408a6fe
Colin <4transparency@naver.com>
2020-01-07 17:56:58 +0900

    [#30] Member Class  name variable  게터세터 다시 만들었어요

--- OWL/src/main/java/com/owl/member/dto/Member.java
+++ OWL/src/main/java/com/owl/member/dto/Member.java
@@ -29,11 +29,15 @@ public class Member {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getUserName() {
-		return userName;
+	
+	
+	
+	
+	public String getName() {
+		return name;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setName(String name) {
+		this.name = name;
 	}
 	public String getPassword() {
 		return password;
