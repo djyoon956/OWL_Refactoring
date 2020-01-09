72a7f0bc22ec4bbb8a16e86d7748ae973484fda8
Lee Jeongeun <jelee1223@naver.com>
2020-01-09 12:53:18 +0900

    [#9] 모달창에 회원 정보 뿌리기

---
+++ OWL/src/main/java/com/owl/member/controller/MemberController.java
@@ -0,0 +1,32 @@
+package com.owl.member.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.owl.member.dto.Member;
+import com.owl.member.service.MemberService;
+
+@Controller
+public class MemberController {
+	
+	@Autowired
+	private MemberService service;
+	
+		//회원정보 조회
+		@RequestMapping("/GetMember.do")
+		public String memberConfirm(String email, Model model) throws Exception{
+		try {	
+			//회원정보
+			Member member = service.getMember("qqq@gmail.com");
+			System.out.println("멤버 조회 : " + member);
+			model.addAttribute("member", member);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		return "include/modal/myProfileSetting";
+		}
+
+}
