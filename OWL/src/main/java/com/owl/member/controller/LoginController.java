6acab9550fabc2adfd15f658ab7bed1af1d129c1
Lee Jeongeun <jelee1223@naver.com>
2020-01-10 15:59:38 +0900

    [#53] service와 controller 나누기

--- OWL/src/main/java/com/owl/member/controller/LoginController.java
+++ OWL/src/main/java/com/owl/member/controller/LoginController.java
@@ -37,6 +37,7 @@ import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.scribejava.core.model.OAuth2AccessToken;
 import com.owl.member.dto.Member;
+import com.owl.member.service.GoogleService;
 import com.owl.member.service.KaKaoService;
 import com.owl.member.service.MemberService;
 import com.owl.member.service.NaverService;
@@ -46,8 +47,10 @@ public class LoginController {
 
 	@Autowired
 	private MemberService service;
+	
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	@Autowired
 	private JavaMailSender mailSender;
 
@@ -59,6 +62,9 @@ public class LoginController {
 
 	@Autowired
 	private NaverService naverService;
+	
+	@Autowired
+	private GoogleService googleService;
 	
     @Autowired
     private GoogleOAuth2Template googleOAuth2Template;
@@ -114,40 +120,10 @@ public class LoginController {
 
 	//구글 로그인 Controller
 	@RequestMapping(value = "GoogleLogin.do")
-    public String doSessionAssignActionPage(String code, HttpServletRequest request) throws Exception {
-        System.out.println("나 와야해");
-        //RestTemplate을 사용하여 Access Token 및 profile을 요청한다.
-        RestTemplate restTemplate = new RestTemplate();
-        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
-        parameters.add("code", code);
-        parameters.add("client_id", "266293759218-1i2o19cvv8p80i4q4a9q94d1dner0ksg.apps.googleusercontent.com");
-        parameters.add("client_secret", "Rr4CKVJVPYVDbTlxEkP2LY-K");
-        parameters.add("redirect_uri", googleOAuth2Parameters.getRedirectUri());
-        parameters.add("grant_type", "authorization_code");
-        System.out.println(parameters);
-        	
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(parameters, headers);
-        ResponseEntity<Map> responseEntity = restTemplate.exchange("https://www.googleapis.com/oauth2/v4/token", HttpMethod.POST, requestEntity, Map.class);
-        Map<String, Object> responseMap = responseEntity.getBody();
-
-        // id_token 라는 키에 사용자가 정보가 존재한다.
-        // 받아온 결과는 JWT (Json Web Token) 형식으로 받아온다. 콤마 단위로 끊어서 첫 번째는 현 토큰에 대한 메타 정보, 두 번째는 우리가 필요한 내용이 존재한다.
-        // 세번째 부분에는 위변조를 방지하기 위한 특정 알고리즘으로 암호화되어 사이닝에 사용한다.
-        //Base 64로 인코딩 되어 있으므로 디코딩한다.
- 
-        String[] tokens = ((String)responseMap.get("id_token")).split("\\.");
-        Base64 base64 = new Base64(true);
-        String body = new String(base64.decode(tokens[1]));
-        
-        System.out.println(tokens.length);
-        System.out.println(new String(Base64.decodeBase64(tokens[0]), "utf-8"));
-        System.out.println(new String(Base64.decodeBase64(tokens[1]), "utf-8"));
- 
-        //Jackson을 사용한 JSON을 자바 Map 형식으로 변환
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, String> result = mapper.readValue(body, Map.class);     
+    public String doSessionAssignActionPage(String code, HttpServletRequest request, Model model) throws Exception {
+		Map<String, String> googlemember = googleService.getGoogleUser(code);
+		model.addAttribute("googlemember", googlemember);
+		System.out.println(model);
         return "member/main"; 
     }
 
