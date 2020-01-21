b878e2b039d07dc1ba9f2f63d1be0a5111418b4c
Lee Jeongeun <jelee1223@naver.com>
2020-01-21 14:12:41 +0900

    [#25] 캘린더 스케줄 업데이트 중

--- Project_OWL/src/main/java/com/owl/calendar/controller/SmartCalendarRestController.java
+++ Project_OWL/src/main/java/com/owl/calendar/controller/SmartCalendarRestController.java
@@ -1,9 +1,12 @@
 package com.owl.calendar.controller;
 
 import java.security.Principal;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -77,40 +80,65 @@ public class SmartCalendarRestController {
 	}
 	
 	@RequestMapping(value="UpdateCalendar.do", method = RequestMethod.POST)
-	public boolean updateCalendar(@RequestParam(value = "calendarId") int calendarId, 
-												@RequestParam(value = "title") String title,
-												@RequestParam(value = "location",required = false) String content,
-												@RequestParam(value = "start") String startDate,
-												@RequestParam(value = "end") String endDate,
-												@RequestParam(value = "allDay") boolean allDay,
+	public boolean updateCalendar(@RequestParam(value = "scheduleId") int calIdx, 
+												  @RequestParam(value = "calendarId" ,required = false) String projectIdx, 												  
+												  @RequestParam(value = "title" ,required = false) String title,
+												  @RequestParam(value = "location",required = false) String content,
+												  @RequestParam(value = "start" ,required = false) String startDate,
+												  @RequestParam(value = "end" ,required = false) String endDate,
+												  @RequestParam(value = "allDay" ,required = false) boolean allDay,
 												Principal principal) {
 		boolean result = false;
+		System.out.println("업데이트 캘린더 탐");
+		System.out.println(startDate);
+		System.out.println(endDate);
+		System.out.println(allDay);
 		SmartCalendar calendar = new SmartCalendar();
 		try {
-		
-		if(calendarId == 0) {
-			calendar.setType(CalendarType.MY);
-		}else {
-			calendar.setType(CalendarType.PROJECT);
-			calendar.setProjectIdx(calendarId);
+		if(projectIdx !=null) { // 프로젝트 변경했을 때
+			int chageIdx = Integer.parseInt(projectIdx);
+			calendar.setProjectIdx(chageIdx);
+			if(chageIdx == 0)
+				calendar.setType(CalendarType.MY);
+			else 
+				calendar.setType(CalendarType.PROJECT);
+		}else { // 프로젝트 변경 없을 때
+			calendar.setProjectIdx(-1);
 		}
+			
 		calendar.setTitle(title);
 		calendar.setContent(content);	
-		
-
-		if(allDay) {			
-			calendar.setStartDate(new SimpleDateFormat("yyyy-mm-dd").parse(startDate));
-			calendar.setEndDate(new SimpleDateFormat("yyyy-mm-dd").parse(endDate));		
-			calendar.setAllDay(1);
+		calendar.setCalIdx(calIdx);
+		System.out.println(startDate);
+		System.out.println("index : "+startDate.indexOf("GMT"));
+		int inddex = startDate.indexOf("GMT");
 	
-		}else {
-			calendar.setStartDate(new SimpleDateFormat("yyyy-mm-dd HH:mm").parse(startDate));
-			calendar.setEndDate(new SimpleDateFormat("yyyy-mm-dd HH:mm").parse(endDate));
-			calendar.setAllDay(0);
-		}		
+		System.out.println(startDate.substring(0, inddex));
+		System.out.println(startDate.substring(0, inddex+1));
+		
+		System.out.println(new SimpleDateFormat("E MMM dd yyyy HH:mm:ss").parse(startDate.substring(0, inddex-1)));
+		System.out.println("-------------------------------");
+			/*
+			 * DateFormat dateFormat = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss ZZZZZ",
+			 * Locale.KOREA);
+			 */
+		
+			if(allDay) {
+					calendar.setStartDate(new SimpleDateFormat("yyyy-mm-dd").parse(startDate));
+					calendar.setEndDate(new SimpleDateFormat("yyyy-mm-dd").parse(endDate));		
+					calendar.setAllDay(1);
+			}else{
+				if(!startDate.isEmpty()) {
+					calendar.setStartDate(new SimpleDateFormat("EEE dd MMM yyyy HH:mm:ss z" ,Locale.KOREA).parse(startDate));
+				}else if(!endDate.isEmpty()) {
+					calendar.setEndDate(new SimpleDateFormat("EEE dd MMM yyyy HH:mm:ss z" ,Locale.KOREA).parse(endDate));		
+				}
+				calendar.setAllDay(0);
+			}
+
 		calendar.setEmail(principal.getName());
 		result = service.updateCalendar(calendar);
-		} catch (ParseException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
