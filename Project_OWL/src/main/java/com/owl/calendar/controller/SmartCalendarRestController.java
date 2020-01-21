5a4465126e9766a854fab0bb68dbc605f626e603
Lee Jeongeun <jelee1223@naver.com>
2020-01-21 10:17:39 +0900

    [#25] 캘린더 삭제 기능 완료

--- Project_OWL/src/main/java/com/owl/calendar/controller/SmartCalendarRestController.java
+++ Project_OWL/src/main/java/com/owl/calendar/controller/SmartCalendarRestController.java
@@ -21,7 +21,7 @@ public class SmartCalendarRestController {
 	@Autowired
 	SmartCalendarService service;
 	
-	@RequestMapping(value="insertCalendar.do", method = RequestMethod.POST)
+	@RequestMapping(value="InsertCalendar.do", method = RequestMethod.POST)
 	public boolean insertCalendar(@RequestParam(value = "calendarId") int calendarId, 
 												@RequestParam(value = "title") String title,
 												@RequestParam(value = "location",required = false) String content,
@@ -62,29 +62,58 @@ public class SmartCalendarRestController {
 		return result;
 	}
 
-	@RequestMapping(value="deleteMyCalendar.do", method = RequestMethod.POST)
-	public boolean deleteMyCalendar(Principal principal) {
+	@RequestMapping(value="DeleteCalendar.do", method = RequestMethod.POST)
+	public boolean deleteMyCalendar(@RequestParam(value = "calendarId") int projectIdx) {
 		boolean result = false;
-		result = service.deleteMyCalendar(principal.getName());	
-		System.out.println(result);
+		result = service.deleteCalendar(projectIdx);	
 		return result;
 	}
 	
-	@RequestMapping(value="deleteProjectCalendar.do", method = RequestMethod.POST)
-	public boolean deleteProejctCalendar(@RequestParam(value = "calendarId") int projectIdx, Principal principal) {
-		boolean result = false;
-		System.out.println(projectIdx);
-		result = service.deleteProjectCalendar(principal.getName(), projectIdx);
-		System.out.println(result);
-		return result;
-	}
-	
-	
-	@RequestMapping(value="getMyAllCalendars.do")
+	@RequestMapping(value="GetMyAllCalendars.do")
 	public List<SmartCalendar> getMyAllCalendars(Principal principal){
 		List<SmartCalendar> calendar = null;
 		calendar = service.getMyAllCalendars(principal.getName());		
-		System.out.println(calendar);
 		return calendar;
+	}
+	
+	@RequestMapping(value="UpdateCalendar.do", method = RequestMethod.POST)
+	public boolean updateCalendar(@RequestParam(value = "calendarId") int calendarId, 
+												@RequestParam(value = "title") String title,
+												@RequestParam(value = "location",required = false) String content,
+												@RequestParam(value = "start") String startDate,
+												@RequestParam(value = "end") String endDate,
+												@RequestParam(value = "allDay") boolean allDay,
+												Principal principal) {
+		boolean result = false;
+		SmartCalendar calendar = new SmartCalendar();
+		try {
+		
+		if(calendarId == 0) {
+			calendar.setType(CalendarType.MY);
+		}else {
+			calendar.setType(CalendarType.PROJECT);
+			calendar.setProjectIdx(calendarId);
+		}
+		calendar.setTitle(title);
+		calendar.setContent(content);	
+		
+
+		if(allDay) {			
+			calendar.setStartDate(new SimpleDateFormat("yyyy-mm-dd").parse(startDate));
+			calendar.setEndDate(new SimpleDateFormat("yyyy-mm-dd").parse(endDate));		
+			calendar.setAllDay(1);
+	
+		}else {
+			calendar.setStartDate(new SimpleDateFormat("yyyy-mm-dd HH:mm").parse(startDate));
+			calendar.setEndDate(new SimpleDateFormat("yyyy-mm-dd HH:mm").parse(endDate));
+			calendar.setAllDay(0);
+		}		
+		calendar.setEmail(principal.getName());
+		result = service.updateCalendar(calendar);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return result;
 	}
 }
