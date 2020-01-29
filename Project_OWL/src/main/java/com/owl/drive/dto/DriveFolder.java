548b59b4917a654721a6487ecc651d434a4c21e6
Lee Jeongeun <jelee1223@naver.com>
2020-01-29 15:20:06 +0900

    [#26] deleteDate

--- Project_OWL/src/main/java/com/owl/drive/dto/DriveFolder.java
+++ Project_OWL/src/main/java/com/owl/drive/dto/DriveFolder.java
@@ -1,5 +1,8 @@
 package com.owl.drive.dto;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,4 +17,13 @@ public class DriveFolder {
 	private int ref;
 	private int depth;
 	private boolean isDelete;	
+	private Date deleteDate;
+	
+	public String getDeleteDate() {
+		String result = "";
+		if (deleteDate != null)
+			result = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(deleteDate);
+		
+		return result;
+	}
 }
