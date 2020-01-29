ac9056331c75f7fd4e8a80eec55e9c8c3fae7caf
euna.chung <local456a@gmail.com>
2020-01-29 17:15:08 +0900

    [#79] ing

--- Project_OWL/src/main/webapp/resources/js/kanban.js
+++ Project_OWL/src/main/webapp/resources/js/kanban.js
@@ -54,6 +54,11 @@ function initKanban(projectIdx){
 		$('.labelList').find('.edit').removeClass("hidden");
 
 	});
+	
+	$("#editIssueDetailBtn").click(function() {
+		console.log("edit 클릭 됨");
+		changeKanbanView("edit");
+	});
 }
 
 function addLabel(lbidx, lbcolor, lbnm) {
@@ -321,11 +326,17 @@ function closeIssue(issueIdx) {
 function changeKanbanView(view){
    if(view == "list"){
    $("#kanbanDetailBox").addClass("hidden");
+   $("#kanbanEditBox").addClass("hidden");
    $("#kanbanMainBox").removeClass("hidden");
 }else if(view == "detail"){
    $("#kanbanMainBox").addClass("hidden");
+   $("#kanbanEditBox").addClass("hidden");
    $("#kanbanDetailBox").removeClass("hidden");
-   }
+}else if (view == "edit"){
+   $("#kanbanMainBox").addClass("hidden");
+   $("#kanbanDetailBox").addClass("hidden");	
+   $("#kanbanEditBox").removeClass("hidden");
+ }
 }
 
 function editLabel(idx, color, name) {
@@ -410,6 +421,4 @@ function editLabel(idx, color, name) {
 	
 
 	
-	$("#editIssueDetailBtn").click(function() {
-		
-	});
+	
